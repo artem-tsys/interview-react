b0a5dcedb5891b897aacbff8a26f1eb2da8329ee
Artem Tsys <19successful91@gmail.com>
2023-02-04 21:44:30 +0200

    finaly settings

--- state/store/middleware.js
+++ state/store/middleware.js
@@ -1,5 +1,4 @@
 import {exampleMiddleware} from '../concepts/example.middleware';
-import {composeWithDevTools} from 'redux-devtools-extension';
 
 export const middlewares = getDefaultMiddleware =>
-  getDefaultMiddleware().concat([composeWithDevTools, exampleMiddleware]);
+  getDefaultMiddleware().concat([exampleMiddleware]);
